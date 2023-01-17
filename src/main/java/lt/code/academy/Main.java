b525f0c65decee40c4342fe834d30eb7b0c1e0f0
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-17 19:07:22 +0200

    jasonToFile

--- src/main/java/lt/code/academy/Main.java
+++ src/main/java/lt/code/academy/Main.java
@@ -1,5 +1,52 @@
-package lt.code.academy;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package lt.code.academy;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT); // atvaizduos graziau
+
+        User user = new User("Rimas", "Spakauskas", 45);
+        User secondUser = new User("Petras", "Pethshyts", 54);
+
+        File file = new File("users.json");
+        if(!file.exists()) {
+
+            //write object tofile
+            file.createNewFile();
+        }
+        mapper.writeValue(file, user);
+
+        //nuskaitom objekta atgal - read object from file
+
+        User readValue = mapper.readValue(file, User.class);
+        System.out.println(readValue);
+
+        //string to object
+        String stringUser = mapper.writeValueAsString(secondUser);
+        System.out.println(stringUser);
+
+        //string tp object
+        User objectUser = mapper.readValue(stringUser, User.class);
+        System.out.println(objectUser);
+
+        //write objects to jason file
+        mapper.writeValue(file, List.of(user, secondUser, objectUser));
+
+        //read object from file 17 d 19 valanda vid ndndndndndndndn
+       List<User>users = mapper.readValue(file, new TypeReference<>() {
+        });
+        System.out.println(users);
+
+
+
     }
 }
---
+++ src/main/java/lt/code/academy/User.java
@@ -0,0 +1,13 @@
+package lt.code.academy;
+
+public record User (String name, String surname, int age){
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
