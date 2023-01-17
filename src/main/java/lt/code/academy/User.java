d49d06c21159cca7c9896382074a60d529de1a62
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-17 20:36:10 +0200

    jasonToFile

--- src/main/java/lt/code/academy/User.java
+++ src/main/java/lt/code/academy/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+//paziureti , yra ir be overaido commite
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
